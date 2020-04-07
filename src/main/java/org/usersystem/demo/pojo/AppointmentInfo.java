0e17dcb7de9f559863ae96dd49b9a62a87d1f246
0xMilk <1144502582@qq.com>
2020-04-07 19:31:16 +0800

    预约信息

--- src/main/java/org/usersystem/demo/pojo/AppointmentInfo.java
+++ src/main/java/org/usersystem/demo/pojo/AppointmentInfo.java
@@ -3,10 +3,12 @@ package org.usersystem.demo.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 //预约信息
 public class AppointmentInfo {
     private String reserve_id;//预约id
