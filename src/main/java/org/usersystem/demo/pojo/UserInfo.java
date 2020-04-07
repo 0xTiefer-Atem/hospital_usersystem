b48580b947aca5f051735ee3f6f98a02e2a6aa3a
0xMilk <1144502582@qq.com>
2020-04-07 19:35:52 +0800

    用户信息

--- src/main/java/org/usersystem/demo/pojo/UserInfo.java
+++ src/main/java/org/usersystem/demo/pojo/UserInfo.java
@@ -3,20 +3,19 @@ package org.usersystem.demo.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 //用户注册信息
 public class UserInfo {
-    private String user_id;//唯一ID
-    private String user_name;//姓名
-    private String user_sex;//性别
-    private String user_tel;//电话
-    private String card_id;//用户身份证
-    private String user_email;//邮件
-    private String user_pwd;//密码
-    private String user_birth;//出生日期
-    private String user_create;//创建时间
-    private String create_time;//数据写入时间
+    private String userId;//唯一ID
+    private String userName;//姓名
+    private String userSex;//性别
+    private String userTel;//电话
+    private String userEmail;//邮件
+    private String userPwd;//密码
+    private String createTime;//数据写入时间
 }
