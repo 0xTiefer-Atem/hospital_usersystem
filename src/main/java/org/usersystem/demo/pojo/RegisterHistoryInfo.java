284528af46f2c13e269066f4f2c6b6258b1a550f
0xMilk <1144502582@qq.com>
2020-04-07 11:56:57 +0800

    挂号历史类

---
+++ src/main/java/org/usersystem/demo/pojo/RegisterHistoryInfo.java
@@ -0,0 +1,27 @@
+package org.usersystem.demo.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterHistoryInfo {
+    private String register_id;//挂号id
+    private String reserve_id;//预约id
+    private String treat_status;//排队状态
+    private String register_time;//登记挂号时间
+    private String user_id;//用户id
+    private String user_tel;//用户电话
+    private String illness_content;//病情简述
+    private String dep_id;//科室id
+    private String dep_name;//科室名字
+    private String staff_id;//职工id
+    private String staff_name;//职工姓名
+    private String reserve_time;//预约时间
+    private String user_name;//用户名
+    private String create_time;//数据写入时间
+
+}
