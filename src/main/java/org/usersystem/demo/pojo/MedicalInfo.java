d33b58eaa6d9d3491bf27b0860bf54457f99a423
0xMilk <1144502582@qq.com>
2020-04-07 19:34:41 +0800

    药品信息

--- src/main/java/org/usersystem/demo/pojo/MedicalInfo.java
+++ src/main/java/org/usersystem/demo/pojo/MedicalInfo.java
@@ -3,10 +3,12 @@ package org.usersystem.demo.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class MedicalInfo {
     private String medical_id;//药物id
     private String medical_name;//药物名称
