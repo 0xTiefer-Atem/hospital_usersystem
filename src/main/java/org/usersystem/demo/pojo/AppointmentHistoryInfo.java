ab22833cfe7d553bee73ffb2e9bc2f0c5595288d
0xMilk <1144502582@qq.com>
2020-04-07 19:30:56 +0800

    预约历史

--- src/main/java/org/usersystem/demo/pojo/AppointmentHistoryInfo.java
+++ src/main/java/org/usersystem/demo/pojo/AppointmentHistoryInfo.java
@@ -3,10 +3,12 @@ package org.usersystem.demo.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class AppointmentHistoryInfo {
     private String reserve_id;//预约id
     private String user_id;//用户id
