36910a87bae3bc3e778d8b2b2980d18cb7ac89ed
0xMilk <1144502582@qq.com>
2020-04-07 19:26:15 +0800

    客户端

---
+++ src/main/java/org/usersystem/demo/controller/ClientControl.java
@@ -0,0 +1,51 @@
+package org.usersystem.demo.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.usersystem.demo.dao.UserDao;
+import org.usersystem.demo.opt.*;
+import org.usersystem.demo.pojo.UserInfo;
+
+import javax.annotation.Resource;
+import java.util.*;
+
+@Controller
+@CrossOrigin
+public class ClientControl {
+
+    @Resource
+    UserDao userDao;
+
+    @RequestMapping(value = "/register/user",method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseV2 registerUser(@RequestBody UserInfo userInfo){
+        userInfo.setCreateTime(TimeOpt.getCurrentTime());
+        userInfo.setUserId(GetUUID.getUUID());
+        System.out.println(userInfo);
+        try{
+            userDao.insertUser(userInfo);
+            return ResponseHelper.create(userInfo, 200, "注册成功！请登录");
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            return ResponseHelper.create( 500, "注册失败");
+        }
+    }
+
+    @PostMapping(value = "/login/identify")
+    @ResponseBody
+    public ResponseV2 loginUser(@RequestBody Map paraMap){
+        System.out.println(paraMap);
+        String userAccount = (String) paraMap.get("userAccount");
+        String userPwd = (String) paraMap.get("userPwd");
+        UserInfo userInfo = userDao.userExists(userAccount);
+        if(userInfo != null){
+            if(userPwd.equals(userInfo.getUserPwd())){
+                return ResponseHelper.create(userInfo.getUserId(), 200, "验证成功");
+            }
+            return ResponseHelper.create(500, "密码错误!");
+        }
+        return ResponseHelper.create(500, "用户不存在!请注册!");
+    }
+}
