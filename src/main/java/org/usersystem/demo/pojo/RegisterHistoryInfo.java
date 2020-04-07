74e1fbd4ecd9e0ef6dbe8fa71c321fbc947527fd
0xMilk <1144502582@qq.com>
2020-04-07 19:35:09 +0800

    挂号历史

--- src/main/java/org/usersystem/demo/pojo/RegisterHistoryInfo.java
+++ src/main/java/org/usersystem/demo/pojo/RegisterHistoryInfo.java
@@ -4,10 +4,12 @@ package org.usersystem.demo.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class RegisterHistoryInfo {
     private String register_id;//挂号id
     private String reserve_id;//预约id
