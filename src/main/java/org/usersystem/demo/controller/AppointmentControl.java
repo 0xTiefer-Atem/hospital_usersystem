8abe17feef91f81a2e80657cbca8d8dbc326e24c
0xMilk <1144502582@qq.com>
2020-04-07 19:25:15 +0800

    预约界面

--- src/main/java/org/usersystem/demo/controller/AppointmentControl.java
+++ src/main/java/org/usersystem/demo/controller/AppointmentControl.java
@@ -4,12 +4,15 @@ import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.usersystem.demo.dao.BlackListDao;
 import org.usersystem.demo.dao.AppointmentDao;
 import org.usersystem.demo.opt.*;
+import org.usersystem.demo.pojo.BlackListInfo;
 import org.usersystem.demo.pojo.DoctorInfo;
 import org.usersystem.demo.pojo.AppointmentHistoryInfo;
 import org.usersystem.demo.pojo.AppointmentInfo;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,9 +21,11 @@ import java.util.Map;
 @CrossOrigin
 public class AppointmentControl {
 
-    @Autowired
-    AppointmentDao reserveDao;
+    @Resource
+    AppointmentDao appointmentDao;
 
+    @Resource
+    BlackListDao blackListDao;
 
     private static String[] morningTimes = {" 08:00:00"," 09:00:00"," 10:00:00"};
     private static String[] afternoonTimes = {" 13:00:00"," 14:00:00"," 15:00:00"};
@@ -30,19 +35,30 @@ public class AppointmentControl {
     @ResponseBody
     public ResponseV2 addReserve(@RequestBody JSONObject jsonObject) {
         String user_id = jsonObject.getString("user_id");
+        BlackListInfo blackListInfo = blackListDao.searchBalckUserById(user_id);
         String reserve_date = jsonObject.getString("reserve_time");
+        if (blackListInfo != null) {
+            String end_time = blackListInfo.getEnd_time();
+            boolean is_late = TimeOpt.compareDate(reserve_date + " 01:00:00", end_time);
+            if (!is_late) {
+                return ResponseHelper.create(null, 10011, "目前处于禁止预约状态,解禁时间为:\t" + end_time.split(" ")[0]);
+            } else {
+                blackListDao.deleteBlackUser(user_id);
+            }
+        }
 
         Map<String, String> paraMap = new HashMap();
-        AppointmentInfo reserveInfo = new AppointmentInfo();
+        AppointmentInfo appointmentInfo = new AppointmentInfo();
         String reserve_id = GetUUID.getUUID();
         String create_time = TimeOpt.getCurrentTime();
 
-        reserveInfo.setReserve_id(reserve_id);
-        reserveInfo.setUser_id(user_id);
-        reserveInfo.setDep_id(jsonObject.getString("dep_id"));
-        reserveInfo.setStaff_id(jsonObject.getString("staff_id"));
-        reserveInfo.setReserve_status("SUCCESS");
-        reserveInfo.setCreate_time(create_time);
+        appointmentInfo.setReserve_id(reserve_id);
+        appointmentInfo.setUser_id(user_id);
+//        reserveInfo.setIllness_content(jsonObject.getString("illness_content"));
+        appointmentInfo.setDep_id(jsonObject.getString("dep_id"));
+        appointmentInfo.setStaff_id(jsonObject.getString("staff_id"));
+        appointmentInfo.setReserve_status("SUCCESS");
+        appointmentInfo.setCreate_time(create_time);
         int flag = jsonObject.getInteger("time");
 
         ResponseV2 responseV2 = null;
@@ -52,44 +68,44 @@ public class AppointmentControl {
             paraMap.put("staff_id", jsonObject.getString("staff_id"));
             paraMap.put("work_time1", reserve_date + " 08:00:00");
             paraMap.put("work_time2", reserve_date + " 12:00:00");
-            int reserve_num = reserveDao.getReserveNum(paraMap);
+            int reserve_num = appointmentDao.getReserveNum(paraMap);
             if (reserve_num == 0) {
                 String reserve_time = reserve_date + morningTimes[0];
-                reserveInfo.setReserve_time(reserve_time);
-                reserveDao.addReserve(reserveInfo);
+                appointmentInfo.setReserve_time(reserve_time);
+                appointmentDao.addReserve(appointmentInfo);
                 responseV2 = ResponseHelper.create(null, 200, "您已预约成功,时间为: "+reserve_date + morningTimes[0]);
             } else if (reserve_num == 1) {
                 String reserve_time = reserve_date + morningTimes[1];
-                reserveInfo.setReserve_time(reserve_time);
-                reserveDao.addReserve(reserveInfo);
+                appointmentInfo.setReserve_time(reserve_time);
+                appointmentDao.addReserve(appointmentInfo);
                 responseV2 = ResponseHelper.create(null, 200, "您已预约成功,时间为: " +reserve_date+ morningTimes[1]);
             } else if (reserve_num == 2) {
                 String reserve_time = reserve_date + morningTimes[2];
-                reserveInfo.setReserve_time(reserve_time);
-                reserveDao.addReserve(reserveInfo);
+                appointmentInfo.setReserve_time(reserve_time);
+                appointmentDao.addReserve(appointmentInfo);
                 responseV2 = ResponseHelper.create(null, 200, "您已预约成功,时间为: " +reserve_date+ morningTimes[2]);
             } else if (reserve_num == 3) {
                 responseV2 = ResponseHelper.create("", 1007, "该医生这天上午预约已满,请换个时间预约");
             }
         } else if (flag == 2) {
-            paraMap.put("staff_id", reserveInfo.getStaff_id());
+            paraMap.put("staff_id", appointmentInfo.getStaff_id());
             paraMap.put("work_time1", reserve_date + " 13:00:00");
             paraMap.put("work_time2", reserve_date + " 16:00:00");
-            int reserve_num = reserveDao.getReserveNum(paraMap);
+            int reserve_num = appointmentDao.getReserveNum(paraMap);
             if (reserve_num == 0) {
                 String reserve_time = reserve_date + afternoonTimes[0];
-                reserveInfo.setReserve_time(reserve_time);
-                reserveDao.addReserve(reserveInfo);
+                appointmentInfo.setReserve_time(reserve_time);
+                appointmentDao.addReserve(appointmentInfo);
                 responseV2 = ResponseHelper.create(null, 200, "您已预约成功,时间为: " +reserve_date+ afternoonTimes[0]);
             } else if (reserve_num == 1) {
                 String reserve_time = reserve_date + afternoonTimes[1];
-                reserveInfo.setReserve_time(reserve_time);
-                reserveDao.addReserve(reserveInfo);
+                appointmentInfo.setReserve_time(reserve_time);
+                appointmentDao.addReserve(appointmentInfo);
                 responseV2 = ResponseHelper.create(null, 200, "您已预约成功,时间为: "+reserve_date + afternoonTimes[1]);
             } else if (reserve_num == 2) {
                 String reserve_time = reserve_date + afternoonTimes[2];
-                reserveInfo.setReserve_time(reserve_time);
-                reserveDao.addReserve(reserveInfo);
+                appointmentInfo.setReserve_time(reserve_time);
+                appointmentDao.addReserve(appointmentInfo);
                 responseV2 = ResponseHelper.create(null, 200, "您已预约成功,时间为: "+reserve_date + afternoonTimes[2]);
             } else if (reserve_num == 3) {
                 responseV2 = ResponseHelper.create("", 1007, "该医生这天下午预约已满,请换个时间预约");
@@ -107,14 +123,16 @@ public class AppointmentControl {
         String user_id =(String) map.get("user_id");
         HashMap hashMap = new HashMap();
         hashMap.put("user_id", user_id);
-        List<AppointmentHistoryInfo> reserveList = reserveDao.searchReserveHistory(hashMap);
+        List<AppointmentHistoryInfo> reserveList = appointmentDao.searchReserveHistory(hashMap);
 
         JSONObject jobj = new JSONObject();
         jobj.put("code",0);
         jobj.put("msg","");
         jobj.put("count",reserveList.size());
         jobj.put("data",reserveList);
+//        System.out.println(jobj.toString());
         return jobj.toString();
+//        return ResponseHelper.create(reserveList, 200, "历史预约信息查询成功");
     }
 
     //一条预约详情
@@ -122,8 +140,8 @@ public class AppointmentControl {
     @ResponseBody
     public ResponseV2 reserveInfoDetail(@RequestBody JSONObject para) {
         String reserve_id = para.getString("reserve_id");
-        AppointmentInfo reserveInfo = reserveDao.reserveInfoDetail(reserve_id);
-        return ResponseHelper.create(reserveInfo, 200, "详细预约信息查询成功");
+        AppointmentInfo appointmentInfo = appointmentDao.reserveInfoDetail(reserve_id);
+        return ResponseHelper.create(appointmentInfo, 200, "详细预约信息查询成功");
     }
 
     //取消预约消息
@@ -135,7 +153,7 @@ public class AppointmentControl {
         Map<String,String> paraMap = new HashMap<>();
         paraMap.put("user_id",user_id);
         paraMap.put("reserve_id",reserve_id);
-        reserveDao.deleteReserve(paraMap);
+        appointmentDao.deleteReserve(paraMap);
         return ResponseHelper.create(null, 200, "取消预约信息成功,请重新预约");
     }
 
@@ -156,12 +174,12 @@ public class AppointmentControl {
            futureTime = TimeOpt.getFetureDate(1).split(" ")[0];
             paraMap.put("dep_id",dep_id);
             paraMap.put("future_time",futureTime);
-            doctorInfoList = reserveDao.getWorkTimeToday(paraMap);
+            doctorInfoList = appointmentDao.getWorkTimeToday(paraMap);
         }else {
             futureTime = TimeOpt.getFetureDate(7).split(" ")[0];
             paraMap.put("dep_id",dep_id);
             paraMap.put("future_time",futureTime);
-            doctorInfoList = reserveDao.getWorkTimeSevenDays(paraMap);
+            doctorInfoList = appointmentDao.getWorkTimeSevenDays(paraMap);
         }
 
         for (DoctorInfo doctorInfo: doctorInfoList) {
@@ -172,10 +190,10 @@ public class AppointmentControl {
                 paraMap.put("staff_id", staff_id);
                 paraMap.put("work_time1", reserve_date + " 08:00:00");
                 paraMap.put("work_time2", reserve_date + " 12:00:00");
-                int am_num = reserveDao.getReserveNum(paraMap);
+                int am_num = appointmentDao.getReserveNum(paraMap);
                 paraMap.put("work_time1", reserve_date + " 13:00:00");
                 paraMap.put("work_time2", reserve_date + " 16:00:00");
-                int pm_num = reserveDao.getReserveNum(paraMap);
+                int pm_num = appointmentDao.getReserveNum(paraMap);
                 System.out.println(am_num + "   " + pm_num);
                 if (am_num == 3) {
                     doctorInfo.setAm(0);
