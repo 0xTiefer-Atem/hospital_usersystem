c67e98c9910b41f58c81c8f9d92706467302839c
0xMilk <1144502582@qq.com>
2020-04-07 19:31:48 +0800

    病例信息

---
+++ src/main/java/org/usersystem/demo/pojo/CaseInfo.java
@@ -0,0 +1,27 @@
+package org.usersystem.demo.pojo;
+
+import com.alibaba.fastjson.JSONArray;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//病例信息
+//多条为看病历史
+@ToString
+public class CaseInfo {
+    private String CaseId;//病例id
+    private String userName;//用户姓名
+    private String userIllness;//病情描述
+    private String staffName;//治疗医生姓名
+    private transient String medicList;//药物清单
+    private float totalPrice;//总缴费金额
+    private String payStatus;//缴费状态 UNPAY是未缴费，PAY是已缴费
+    private String createTime;//病例生成日期
+    private JSONArray medicListJson;//药物清单json格式
+}
