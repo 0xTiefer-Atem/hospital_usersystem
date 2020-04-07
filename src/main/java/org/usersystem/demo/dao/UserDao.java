22436d406981acb09129d073378ea4e08d47d814
0xMilk <1144502582@qq.com>
2020-04-07 19:29:25 +0800

    用户操作

--- src/main/java/org/usersystem/demo/dao/UserDao.java
+++ src/main/java/org/usersystem/demo/dao/UserDao.java
@@ -8,13 +8,13 @@ import org.usersystem.demo.pojo.UserInfo;
 
 @Mapper
 public interface UserDao {
-    @Insert("insert into user_info " +
-            "(user_id, user_name, user_sex, " +
-            "user_tel,user_email, user_pwd, " +
-            "user_birth, user_create,create_time,card_id)" +
-            "values(#{user_id},#{user_name},#{user_sex}," +
-            "#{user_tel},#{user_email},#{user_pwd}," +
-            "#{user_birth},#{user_create},#{create_time},#{card_id}) ")
+    @Insert("insert into userInfo " +
+            "(userId, userName, userSex, " +
+            "userTel,userEmail, userPwd, " +
+            "createTime)" +
+            "values(#{userId},#{userName},#{userSex}," +
+            "#{userTel},#{userEmail},#{userPwd}," +
+            "#{createTime}) ")
     void insertUser(UserInfo userInfo);
 
     @Select("select user_pwd from user_info where user_email=#{user_email}")
@@ -23,12 +23,6 @@ public interface UserDao {
     @Select("select user_pwd from user_info where user_tel=#{user_tel}")
     String searchPwdByTel(String user_tel);
 
-    @Select("select count(*) from user_info where user_email = #{user_email}")
-    int userExists(String user_email);
-
-    @Select("select user_id from user_info where user_email = #{user_email}")
-    String searchIdByEmail(String user_eamil);
-
-    @Select("select user_id from user_info where user_tel = #{user_tel}")
-    String searchIdByTel(String user_tel);
+    @Select("select * from userInfo where userEmail = #{userEmail} ")
+    UserInfo userExists(String userEmail);
 }
