b6ffa034558d2c9ad8157b53e1d863049c3dc653
0xMilk <1144502582@qq.com>
2020-04-07 11:56:15 +0800

    病例信息类

---
+++ src/main/java/org/usersystem/demo/pojo/IllnessInfo.java
@@ -0,0 +1,24 @@
+package org.usersystem.demo.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//病例信息
+//多条为看病历史
+public class IllnessInfo {
+    private String illness_id;//病例id
+    private String user_name;//用户姓名
+    private String illness_desc;//病情描述
+    private String staff_name;//治疗医生姓名
+    private String medical_id_num;//药物清单(药物id,数量,单价)
+    private float total_money;//总缴费金额
+    private String pay_status;//缴费状态 -1未缴费  0已缴费
+    private String illness_time;//病例生成日期
+    private List<MedicalInfo> medicalInfoList;//药品列表
+}
