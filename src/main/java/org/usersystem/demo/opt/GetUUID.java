b244c7bf518592368a12de8628415220d7030a57
0xMilk <1144502582@qq.com>
2020-04-07 11:48:36 +0800

    随机id类

---
+++ src/main/java/org/usersystem/demo/opt/GetUUID.java
@@ -0,0 +1,12 @@
+package org.usersystem.demo.opt;
+
+
+import java.util.Date;
+import java.util.UUID;
+
+public class GetUUID {
+    private static Date date = new Date();
+    public static String getUUID(){
+        return UUID.randomUUID().toString().replace("-","").subSequence(1,5).toString()+date.getTime();
+    }
+}
