0d1d3584e260b9afa039ad69945e22bef7cd97a9
0xMilk <1144502582@qq.com>
2020-04-14 23:39:30 +0800

    添加查询职员信息语句

--- src/main/java/org/usersystem/demo/dao/OtherDao.java
+++ src/main/java/org/usersystem/demo/dao/OtherDao.java
@@ -4,6 +4,7 @@ package org.usersystem.demo.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.usersystem.demo.pojo.CliInfo;
+import org.usersystem.demo.pojo.StaffInfo;
 
 import java.util.List;
 
@@ -11,4 +12,8 @@ import java.util.List;
 public interface OtherDao {
     @Select("select cliId,cliName from clinicInfo")
     List<CliInfo> getCliInfo();
+
+    @Select("select staffId,staffName from staffInfo where staffId like CONCAT(#{id},'%') ")
+    List<StaffInfo> getStaffInfo(String id);
+
 }
