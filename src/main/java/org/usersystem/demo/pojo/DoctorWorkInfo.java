459ed7f9a70aa276e3577b1dd3736eddaa469340
0xMilk <1144502582@qq.com>
2020-04-17 22:28:38 +0800

    修改变量类型

--- src/main/java/org/usersystem/demo/pojo/DoctorWorkInfo.java
+++ src/main/java/org/usersystem/demo/pojo/DoctorWorkInfo.java
@@ -10,7 +10,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class DoctorWorkInfo {
-    private String appointmentTime;
-    private String morningNum;
-    private String afternoonNum;
+    private String appointmentTime;//已预约的时间
+    private int morningNum; //上午预约几个
+    private int afternoonNum; //下午预约几个
 }
