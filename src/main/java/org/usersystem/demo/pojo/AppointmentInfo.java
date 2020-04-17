167ba0c803fc2aee121dc38801c66ee0c3bbebfa
0xMilk <1144502582@qq.com>
2020-04-17 22:28:23 +0800

    修改变量

--- src/main/java/org/usersystem/demo/pojo/AppointmentInfo.java
+++ src/main/java/org/usersystem/demo/pojo/AppointmentInfo.java
@@ -11,15 +11,11 @@ import lombok.ToString;
 @ToString
 //预约信息
 public class AppointmentInfo {
-    private String reserve_id;//预约id
-    private String user_id;//用户id
-    private String illness_content;//病情
-    private String dep_id;//科室
-    private String staff_id;//预约医生id
-    private String staff_name;//预约医生姓名
-    private String flag;//来判断是上午预约还是下午
-    private String reserve_time;//预约时间
-    private String reserve_status;//状态 -1未预约成功 0预约成功 1该预约已完成 1001等待预约 1002用户自己取消预约
-    private String create_time;//数据写入时间
+    private String appointmentId;//预约id
+    private String userId;//用户id
+    private String staffId;//预约医生id
+    private String appointmentTime;//预约时间
+    private String status;//状态
+    private String createTime;//数据写入时间
 
 }
