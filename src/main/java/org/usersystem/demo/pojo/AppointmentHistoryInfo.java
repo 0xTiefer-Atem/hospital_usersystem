93837de324c53edd2f41ec80b396aeea37970a8f
0xMilk <1144502582@qq.com>
2020-04-19 00:04:16 +0800

    修改变量

--- src/main/java/org/usersystem/demo/pojo/AppointmentHistoryInfo.java
+++ src/main/java/org/usersystem/demo/pojo/AppointmentHistoryInfo.java
@@ -10,13 +10,11 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class AppointmentHistoryInfo {
-    private String reserve_id;//预约id
-    private String user_id;//用户id
-    private String user_name;//用户名
-    private String user_tel;//用户电话
-    private String illness_content;//病情描述
-    private String reserve_time;//预约时间
-    private String reserve_status;//预约状态
-    private String staff_name;//职工姓名
-    private String dep_name;//科室名称
+    private String appointmentId;//预约id
+    private String appointmentTime;//预约时间
+    private String status;//预约状态
+    private String staffId;//职员Id
+    private String staffName;//职工姓名
+    private String cliId;//科室id
+    private String cliName;//科室名称
 }
