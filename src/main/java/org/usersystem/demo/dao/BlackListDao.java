592e76e23a2cf4f74712bc47a57f5bb007b1b5ad
0xMilk <1144502582@qq.com>
2020-04-20 23:33:12 +0800

    添加注释

--- src/main/java/org/usersystem/demo/dao/BlackListDao.java
+++ src/main/java/org/usersystem/demo/dao/BlackListDao.java
@@ -1,20 +1,20 @@
-package org.usersystem.demo.dao;
-
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.usersystem.demo.pojo.BlackListInfo;
-
-@Mapper
-public interface BlackListDao {
-    @Insert("insert into blacklist_info(user_id,start_time,end_time)" +
-            " values(#{user_id},#{start_time},#{end_time})")
-    void insterBlackUser(BlackListInfo blackListInfo);
-
-    @Delete("delete from blacklist_info where user_id = #{user_id}")
-    void deleteBlackUser(String user_id);
-
-    @Select("select * from blacklist_info where user_id = #{user_id}")
-    BlackListInfo searchBalckUserById(String user_id);
-}
+//package org.usersystem.demo.dao;
+//
+//import org.apache.ibatis.annotations.Delete;
+//import org.apache.ibatis.annotations.Insert;
+//import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Select;
+//import org.usersystem.demo.pojo.BlackListInfo;
+//
+//@Mapper
+//public interface BlackListDao {
+//    @Insert("insert into blacklist_info(user_id,start_time,end_time)" +
+//            " values(#{user_id},#{start_time},#{end_time})")
+//    void insterBlackUser(BlackListInfo blackListInfo);
+//
+//    @Delete("delete from blacklist_info where user_id = #{user_id}")
+//    void deleteBlackUser(String user_id);
+//
+//    @Select("select * from blacklist_info where user_id = #{user_id}")
+//    BlackListInfo searchBalckUserById(String user_id);
+//}
