569de1ae016bf6904c4589f14c4f636207448b76
0xMilk <1144502582@qq.com>
2020-04-20 23:34:12 +0800

    添加注释

--- src/main/java/org/usersystem/demo/pojo/MedicalInfo.java
+++ src/main/java/org/usersystem/demo/pojo/MedicalInfo.java
@@ -1,17 +1,17 @@
-package org.usersystem.demo.pojo;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-public class MedicalInfo {
-    private String medical_id;//药物id
-    private String medical_name;//药物名称
-    private float medical_price;//药物价格
-    private String buy_num;//购买数量
-}
+//package org.usersystem.demo.pojo;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+//public class MedicalInfo {
+//    private String medical_id;//药物id
+//    private String medical_name;//药物名称
+//    private float medical_price;//药物价格
+//    private String buy_num;//购买数量
+//}
