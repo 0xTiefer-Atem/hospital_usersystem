0c5d626a7164e3ce5304f57ef1a6c4cc924bb2c7
Wq <1144502582@qq.com>
2021-01-28 19:10:09 +0800

    格式化代码

--- src/main/java/org/usersystem/demo/controller/ClientControl.java
+++ src/main/java/org/usersystem/demo/controller/ClientControl.java
@@ -20,18 +20,18 @@ public class ClientControl {
 
 
     //注册接口
-    @RequestMapping(value = "/register/user",method = RequestMethod.POST)
+    @RequestMapping(value = "/register/user", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseV2 registerUser(@RequestBody UserInfo userInfo){
+    public ResponseV2 registerUser(@RequestBody UserInfo userInfo) {
         userInfo.setCreateTime(TimeOpt.getCurrentTime());
         userInfo.setUserId(GetUUID.getUUID());
         System.out.println(userInfo);
-        try{
+        try {
             userDao.insertUser(userInfo);
             return ResponseHelper.create(userInfo, 200, "注册成功！请登录");
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            return ResponseHelper.create( 500, "注册失败");
+            return ResponseHelper.create(500, "注册失败");
         }
     }
 
@@ -39,13 +39,13 @@ public class ClientControl {
     //登陆接口
     @PostMapping(value = "/login/identify")
     @ResponseBody
-    public ResponseV2 loginUser(@RequestBody Map paraMap){
+    public ResponseV2 loginUser(@RequestBody Map paraMap) {
         System.out.println(paraMap);
         String userAccount = (String) paraMap.get("userAccount");
         String userPwd = (String) paraMap.get("userPwd");
         UserInfo userInfo = userDao.userExists(userAccount);
-        if(userInfo != null){
-            if(userPwd.equals(userInfo.getUserPwd())){
+        if (userInfo != null) {
+            if (userPwd.equals(userInfo.getUserPwd())) {
                 Map map = new HashMap();
                 map.put("userInfo", userInfo);
                 return ResponseHelper.create(map, 200, "验证成功");
